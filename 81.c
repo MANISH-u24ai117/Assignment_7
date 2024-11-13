45287d34ea3a25b1b1779612a6af906a344a62a1
MANISH-u24ai117 <u24ai117@aid.svnit.ac.in>
2024-11-13 09:03:01 +0530

    Update 81.c

--- 81.c
+++ 81.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 
 int isPrime(int num) {
+    int i;
     if (num <= 1) return 0;
-    for (int i = 2; i <= num / 2; i++) {
+    for ( i = 2; i <= num / 2; i++) {
         if (num % i == 0)
             return 0;
     }
